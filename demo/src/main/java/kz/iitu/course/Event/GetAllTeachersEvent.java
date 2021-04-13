356b5200c98f12ea7934e4ced5eedd04171dcbcb
Korlan Omarova <omar_korlan@mail.ru>
2021-04-13 22:58:46 +0600

    commit

--- demo/src/main/java/kz/iitu/course/Event/GetAllTeachersEvent.java
+++
@@ -1,9 +0,0 @@
-package kz.iitu.course.Event;
-
-import org.springframework.context.ApplicationEvent;
-
-public class GetAllTeachersEvent extends ApplicationEvent {
-    public GetAllTeachersEvent(Object source) {
-        super(source);
-    }
-}
--- Новая папка 5/course/src/main/java/kz/iitu/course/Client/Client.java
+++
@@ -1,6 +0,0 @@
-package kz.iitu.course.Client;
-
-public interface Client {
-    String getType();
-    String getName();
-}
